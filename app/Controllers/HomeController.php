88d52aeb2d1ab61ff74ee38e4b4ff97c934a62e0
Diego Hinagas <hinagasrodriguez@gmail.com>
2023-04-11 20:24:28 -0400

    added landing page

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -1,9 +1,28 @@
 <?php
 
-class HomeController
+use App\Core\baseController;
+use App\Models\Event;
+use App\Models\EventParticipant;
+use App\Models\Libro;
+
+class HomeController  extends baseController
 {
     public function Index()
     {
-        echo 'Hello World <br> <a href="?controller=auth">Login</a>';
+        $libro_model = new Libro();
+        $libros = $libro_model->getAll();
+
+        $event_model = new Event();
+        $events = $event_model->getAll();
+
+        $events_pendientes = array_filter($events, fn($event) => (int) $event->state_event === 2);
+
+        usort($events_pendientes, fn($a, $b) => strtotime($a->date_realized_event) - strtotime($b->date_realized_event));
+
+        $this->view('LandingPage/Inicio', [
+            'title' => 'Inicio',
+            'libros' => $libros,
+            'events_pendientes' => $events_pendientes
+        ]);
     }
 }
---
+++ app/Views/LandingPage/Inicio.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Biblioteca Agustín Codazzi</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/img/icono.jpg" />
+        <!-- Font Awesome CDN -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <!-- Bootstrap Icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css" />
+        <!-- SimpleLightbox plugin CSS-->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="assets/css/landing-page.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+            <div class="container px-4 px-lg-5">
+                <a class="navbar-brand" href="#page-top">Inicio</a>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto my-2 my-lg-0">
+                        <li class="nav-item"><a class="nav-link" href="#about">Nosotros</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#services">Servicios</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#books">Libros</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#events">Eventos</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Masthead-->
+        <header class="masthead">
+            <div class="container px-4 px-lg-5 h-100">
+                <div class="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
+                    <div class="col-lg-8 align-self-end">
+                        <h1 class="text-white font-weight-bold">Biblioteca</h1>
+                        <h1 class="text-white font-weight-bold">"Agustín Codazzi"</h1>
+                        <hr class="divider divider-dark" />
+                    </div>
+                    <div class="col-lg-8 align-self-baseline">
+                        <p class="text-white mb-5">Un espacio donde se preserva un registro cultural del pasado y del presente, facilitando el acceso de los usuarios a los conocimientos e incentivando la creación.</p>
+                        <a class="btn btn-dark btn-xl" href="?controller=auth">Ingresar</a>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- About-->
+        <section class="page-section bg-dark" id="about">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-lg-8 text-center">
+                        <h2 class="text-white mt-0">¿Quienes somos?</h2>
+                        <hr class="divider divider-light" />
+                        <p class="text-white-75 mb-4">Somos un centro que rescata, preserva, organiza y difunde el acervo documental estatal y facilita el acceso a una variedad de materiales bibliográficos con el proposito de satisfacer las necesidades de información personal, educacional, profesional y recreacional de los ciudadanos.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Services-->
+        <section class="page-section" id="services">
+            <div class="container px-4 px-lg-5">
+                <h2 class="text-center mt-0">Nuestros servicios</h2>
+                <hr class="divider divider-dark" />
+                <div class="row gx-4 gx-lg-5">
+                    <div class="col-lg-4 col-md-4 text-center">
+                        <div class="mt-5">
+                            <div class="mb-3"><i class="fa-solid fa-file-pen fs-1"></i></div>
+                            <h3 class="h4 mb-2">Lectura e investigación</h3>
+                            <p class="text-muted mb-0">Contamos con una amplia colección de material de referencia, en ciencias, humanidades, literatura y más.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 text-center">
+                        <div class="mt-5">
+                            <div class="mb-3"><i class="fa-solid fa-book-open fs-1"></i></div>
+                            <h3 class="h4 mb-2">Préstamo circulante</h3>
+                            <p class="text-muted mb-0">Se prestan los libros para llevarlos a casa haciendo más cómoda la investigación o la lectura.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 text-center">
+                        <div class="mt-5">
+                            <div class="mb-3"><i class="fa-regular fa-folder-open fs-1"></i></div>
+                            <h3 class="h4 mb-2">Depósito legal</h3>
+                            <p class="text-muted mb-0">En la institución se reciben los ejemplares indicados por la Biblioteca Nacional para su conservación.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Books-->
+        <section class="page-section bg-dark" id="books">
+            <div class="container px-4 px-lg-5">
+                <h2 class="text-center  text-white mt-0">Últimos libros</h2>
+                <hr class="divider divider-light" />
+                <div class="row gx-4 gx-lg-5">
+                <?php foreach($libros as $libro): ?>
+                    <div class="col-lg-3 col-md-4 text-center">
+                        <div class="mt-5">
+                            <div class="mb-3"><i class="fa-solid fa-book text-white fs-1"></i></div>
+                            <h3 class="h4 mb-2 text-white"><?php echo $libro->titulo ?></h3>
+                            <p class="text-muted mb-0"><?php echo $libro->autor ?></p>
+                        </div>
+                    </div>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+        </section>
+        <!-- Events-->
+        <section class="page-section" id="events">
+
+            <div class="container px-4 px-lg-5">
+                <h2 class="text-center mt-0">Próximos eventos</h2>
+                <hr class="divider divider-dark" />
+                <div class="gx-4 gx-lg-5">
+                    <div class="row mt-4">
+                        <div class="col-md-5 col-sm-12">
+
+                            <div class="card shadow mb-3 d-none" id="not-event">
+                                <div class="card-body text-center">
+                                    <h6 class="card-title">No hay ningun evento en esta fecha</h6>
+                                    <button class="btn btn-link p-0" id="view-defaul-events">Ver los eventos disponibles</button>
+                                </div>
+                            </div>
+
+                            <?php foreach($events_pendientes as $event): ?>
+                                <div class="card shadow defaul-events mb-3" id="<?php echo $event->id_event ?>">
+                                    <div class="card-body">
+                                        <div class="row px-2">
+                                            <div class="col-md-2 fw-bold">
+                                                <h3 class="mb-0"><?php echo $helpers->getCustomDate($event->date_realized_event, 'd') ?></h3>
+                                            </div>
+                                            <div class="col-md-3">
+                                                <p class="mb-0 text-secondary">Fecha:</p>
+                                                <h6 class="mt-0 fw-bold"><?php echo $helpers->getCustomDate($event->date_realized_event, 'Y-m') ?></h6>
+
+                                                <p class="mb-0 text-secondary">Hora:</p>
+                                                <h6 class="mt-0 fw-bold"><?php echo $helpers->getCustomDate($event->time, 'h:s') ?></h6>
+                                            </div>
+                                            <div class="col">
+                                                <h5 class="fw-bold"><?php echo $event->title_event ?></h5>
+                                                <p><?php echo $event->place_event ?></p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            <?php endforeach; ?>
+                        </div>
+                        <div class="col-md-6 col-sm-12">
+                            <div class="card shadow">
+                                <div class="card-body">
+                                    <div id="calendar"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+        <!-- Footer-->
+        <footer class="bg-light py-5">
+            <div class="container px-4 px-lg-5"><div class="small text-center text-muted">Copyright &copy; 2023 - Biblioteca Pública Central "Agustín Codazzi"</div></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- SimpleLightbox plugin JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.js"></script>
+
+        <!-- AXIOS -->
+        <script src="../node_modules/axios/dist/axios.min.js"></script>
+
+        <!-- Fullcalendar -->
+        <script src="../node_modules/fullcalendar/index.global.min.js"></script>
+
+        <!-- Core theme JS-->
+        <script src="assets/js/landing-page.js" type="module"></script>
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <!-- * *                               SB Forms JS                               * *-->
+        <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <!-- <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script> -->
+    </body>
+</html>
